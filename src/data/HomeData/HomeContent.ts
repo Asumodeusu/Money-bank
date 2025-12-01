74757fb48b30520b7918a701aa9afc2a7d613e33
Asumodeusu <pop.illya@yandex.ru>
2025-12-01 16:42:44 +0500

    adaptive from tel

--- src/data/HomeData/HomeContent.ts
+++ src/data/HomeData/HomeContent.ts
@@ -16,34 +16,34 @@ export const homeItems: DataContent[] = [
     icon: "/svg/icons/withdraw.svg",
     label: "Withdraw",
   },
-  // {
-  //   id: "mobile",
-  //   icon: "/svg/icons/mobile.svg",
-  //   label: "Mobile prepaid",
-  // },
-  // {
-  //   id: "bill",
-  //   icon: "/svg/icons/bill.svg",
-  //   label: "Pay the bill",
-  // },
-  // {
-  //   id: "save",
-  //   icon: "/svg/icons/pig.svg",
-  //   label: "Save online",
-  // },
-  // // {
-  // //   id: "credit-card",
-  // //   icon: "/svg/icons/credit-card.svg",
-  // //   label: "Creditcard",
-  // // },
-  // // {
-  // //   id: "report",
-  // //   icon: "/svg/icons/transfer.svg",
-  // //   label: "Transaction report",
-  // // },
-  // // {
-  // //   id: "beneficiary",
-  // //   icon: "/svg/icons/Benefecia.svg",
-  // //   label: "Beneficiary",
-  // // },
+  {
+    id: "mobile",
+    icon: "/svg/icons/mobile.svg",
+    label: "Mobile prepaid",
+  },
+  {
+    id: "bill",
+    icon: "/svg/icons/bill.svg",
+    label: "Pay the bill",
+  },
+  {
+    id: "save",
+    icon: "/svg/icons/pig.svg",
+    label: "Save online",
+  },
+  {
+    id: "credit-card",
+    icon: "/svg/icons/credit-card.svg",
+    label: "Creditcard",
+  },
+  {
+    id: "report",
+    icon: "/svg/icons/transfer.svg",
+    label: "Transaction report",
+  },
+  {
+    id: "beneficiary",
+    icon: "/svg/icons/Benefecia.svg",
+    label: "Beneficiary",
+  },
 ];
